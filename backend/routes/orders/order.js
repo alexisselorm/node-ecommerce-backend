a74b88432090c48125fff55c08bed7add9e856dd
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-09-13 07:45:24 +0100

    Dynamically calculating total price of every order

--- backend/routes/orders/order.js
+++ backend/routes/orders/order.js
@@ -7,18 +7,14 @@ const {OrderItem} = require('../../models/orderItems/orderItem');
 router.get('/:id',async (req,res)=>{
     const orders = await Order.findById(req.params.id)
     .populate('user','name')
-    .populate({path:'orderItems',populate:'product'})
-    if (!orders) {
-        res.status(500).json({success:false})
-        
-    }
-    res.send(orders)
+    .populate({path:'orderItems',populate:{path:'product',populate:'category'}})
+    !orders ? res.status(500).json({success:false}) : res.send(orders);
 })
 
 router.get('/',async (req,res)=>{
     const orders = await Order.find().populate('user','name').sort({'dateOrdered':-1})
     if (!orders) {
-        res.status(500).json({success:false})
+       return res.status(500).json({success:false})
         
     }
     res.send(orders)
@@ -35,7 +31,15 @@ router.post('/',async (req,res)=>{
         return newOrderItem._id
     }))
     const orderItemsIdsResolved  = await orderItemsIds;
+    const totalPrices =await Promise.all(orderItemsIdsResolved.map(async(orderItemId)=>{
+    const orderItem = await OrderItem.findById(orderItemId).populate('product','price')
+    const totalPrice = orderItem.product.price * orderItem.quantity
+    console.log(totalPrice);
+    return totalPrice
+}))
+const totalPrice = totalPrices.reduce((accumulator,current)=>accumulator+current,0)
 
+// console.log(totalPrices);
     let order = new Order({
         orderItems:orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -45,12 +49,47 @@ router.post('/',async (req,res)=>{
         phone:req.body.phone,
         country:req.body.country,
         status:req.body.status,
-        totalPrice:req.body.totalPrice,
+        totalPrice:totalPrice,
         user:req.body.user
     })
     order = await order.save()
     if(!order) return res.status(400).send("Order could not be created")
     res.status(201).send(order)
+})
+
+router.put('/:id',async(req,res)=>{
+    let order = await Order.findByIdAndUpdate(req.params.id,{
+        status: req.body.status
+    },{
+        new:true
+    })
+    if(!order) return res.status(401).send("Error with updating the order")
+    res.status(201).send(order)
+})
+
+// My code that works <-- Mostly -->
+// router.delete('/:id', async (req,res)=>{
+//     let order =await Order.findByIdAndRemove(req.params.id, async (orderItem)=>{
+//         await OrderItem.findByIdAndRemove(orderItem)
+//     })
+
+//     return order ? res.status(200).send("Order deleted") : res.status(400).json("Something went wrong");
+// })
+
+// More sophisticated one but not with async
+router.delete('/:id', (req, res)=>{
+    Order.findByIdAndRemove(req.params.id).then(async order =>{
+        if(order) {
+            await Order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            return res.status(200).json({success: true, message: 'the order is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "order not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
 })
 
 module.exports=router
