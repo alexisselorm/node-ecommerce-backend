18782aac3a95c27f596158735e135239d6c15449
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-12 16:45:18 +0200

    use routes

--- node-api-design/src/router.ts
+++ node-api-design/src/router.ts
@@ -22,3 +22,5 @@ router.get("/updatepoint/:id", (req, res) => {});
 router.put("/updatepoint/:id", (req, res) => {});
 router.post("/updatepoint", (req, res) => {});
 router.delete("/updatepoint/:id", (req, res) => {});
+
+export default router;
