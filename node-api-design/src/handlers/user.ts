0d35528faeafc3ef9a1af5d8db45e64694403c89
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-12 22:16:43 +0200

    User signup and sign in

--- node-api-design/src/handlers/user.ts
+++ node-api-design/src/handlers/user.ts
@@ -1,14 +1,35 @@
 import prisma from "../db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
 export const createNewUser = async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: await hashPassword(req.body.password),
-    },
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: await hashPassword(req.body.password),
+      },
+    });
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (error) {
+    res.status(error.status);
+    console.log(error);
+  }
+};
+
+export const signin = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { username: req.body.username },
   });
 
+  const isValid = await comparePasswords(req.body.password, user.password);
+  if (!isValid) {
+    res.status(401);
+    res.json({ message: "Nope. Wrong password" });
+    return;
+  }
+
   const token = createJWT(user);
-  res.json(token);
+  res.json({ token });
 };
