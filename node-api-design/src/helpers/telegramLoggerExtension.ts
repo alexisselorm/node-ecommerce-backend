2541d2f6463c6c1f5797961db9bd3cfd92f0e256
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-13 21:08:34 +0200

    Successfully added telegram logging to winston

---
+++ node-api-design/src/helpers/telegramLoggerExtension.ts
@@ -0,0 +1,33 @@
+import winston from "winston";
+import TelegramBot from "node-telegram-bot-api";
+import TransportStream from "winston-transport";
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_API, { polling: false });
+
+class TelegramTransport extends TransportStream {
+  constructor(options) {
+    super(options);
+    this.level = options.level || "info";
+    this.chatId = options.chatId;
+  }
+
+  public log(info: any, callback) {
+    setImmediate(() => {
+      this.emit("logged", info);
+    });
+
+    // Send log message to Telegram
+    const message = `[${info.level.toUpperCase()}] ${info.message}`;
+    bot.sendMessage(this.chatId, message);
+
+    callback();
+  }
+}
+
+const logger = winston.createLogger({
+  transports: [
+    new TelegramTransport({ level: "info", chatId: process.env.CHAT_ID }),
+  ],
+});
+
+export default logger;
