a73d431d802dd3a0b127b8219589c810fef3107d
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-14 18:20:12 +0200

    Will need a workaround for winston telegram

--- node-api-design/src/helpers/telegramLoggerExtension.ts
+++ node-api-design/src/helpers/telegramLoggerExtension.ts
@@ -2,33 +2,33 @@ import winston from "winston";
 import TelegramBot from "node-telegram-bot-api";
 import TransportStream from "winston-transport";
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_API, { polling: false });
+// const bot = new TelegramBot(process.env.TELEGRAM_BOT_API, { polling: false });
 
-class TelegramTransport extends TransportStream {
-  constructor(options) {
-    super(options);
-    this.level = options.level || "info";
-    this.chatId = options.chatId;
-  }
+// class TelegramTransport extends TransportStream {
+//   constructor(options) {
+//     super(options);
+//     this.level = options.level || "info";
+//     this.chatId = options.chatId;
+//   }
 
-  public log(info: any, callback) {
-    setImmediate(() => {
-      this.emit("logged", info);
-    });
+//   public log(info: any, callback) {
+//     setImmediate(() => {
+//       this.emit("logged", info);
+//     });
 
-    // Send log message to Telegram
-    const message = ` ${new Date().toISOString()} - [${info.level.toUpperCase()}] ${
-      info.message
-    }`;
-    bot.sendMessage(this.chatId, message);
+//     // Send log message to Telegram
+//     const message = ` ${new Date().toISOString()} - [${info.level.toUpperCase()}] ${
+//       info.message
+//     }`;
+//     bot.sendMessage(this.chatId, message);
 
-    callback();
-  }
-}
+//     callback();
+//   }
+// }
 
 const logger = winston.createLogger({
   transports: [
-    new TelegramTransport({ level: "info", chatId: process.env.CHAT_ID }),
+    // new TelegramTransport({ level: "info", chatId: process.env.CHAT_ID }),
     new winston.transports.Console({
       level: "info",
       format: winston.format.combine(
