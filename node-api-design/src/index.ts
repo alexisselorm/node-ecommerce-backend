a574285bd5612f199ffce06b6ce530896dabc9ec
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-14 17:49:51 +0200

    Staging for deployment

--- node-api-design/src/index.ts
+++ node-api-design/src/index.ts
@@ -1,7 +1,8 @@
+import config from "./config";
 import app from "./server";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server listening on http://localhost:3000");
+app.listen(config.port, () => {
+  console.log(`Server listening on http://localhost:${config.port}`);
 });
